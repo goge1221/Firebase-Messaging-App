b7fc8158aae29e17409bc5daa5d34eea581017d6
Goje Andrei <a12032793@univie.ac.at>
2022-05-17 10:07:08 +0200

    Icon and text of Button in the middle changed so that it matches Chat.

--- app/src/main/java/com/example/studdybuddy/ui/Chat/ChatFragment.java
+++ app/src/main/java/com/example/studdybuddy/ui/Chat/ChatFragment.java
@@ -1,4 +1,4 @@
-package com.example.studdybuddy.ui.dashboard;
+package com.example.studdybuddy.ui.Chat;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,20 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.studdybuddy.databinding.FragmentDashboardBinding;
+import com.example.studdybuddy.databinding.FragmentChatBinding;
 
-public class DashboardFragment extends Fragment {
+public class ChatFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentChatBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        ChatViewModel chatViewModel =
+                new ViewModelProvider(this).get(ChatViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentChatBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
---
+++ app/src/main/java/com/example/studdybuddy/ui/Chat/ChatViewModel.java
@@ -0,0 +1,14 @@
+package com.example.studdybuddy.ui.Chat;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ChatViewModel extends ViewModel {
+
+
+    public ChatViewModel() {
+
+    }
+
+}
