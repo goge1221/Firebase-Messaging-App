9abaa1d1fb804923cd8329e2dc67706999a55143
Goje Andrei <a12032793@unet.univie.ac.at>
2022-05-17 16:07:11 +0200

    Connection to firebase established including message receiving and adding into database.

--- app/src/main/java/com/example/studdybuddy/activities/LogInActivity.java
+++ app/src/main/java/com/example/studdybuddy/activities/LogInActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.studdybuddy.R;
 import com.example.studdybuddy.databinding.ActivityLogInBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class LogInActivity extends AppCompatActivity {
 
