2a099f76c9f226fa0610082d210ad54bbc98ea34
Goje Andrei <a12032793@unet.univie.ac.at>
2022-05-19 08:53:16 +0200

    Message ready i think.

--- app/src/main/java/com/example/studdybuddy/CustomAdapter/RecentMessageAdapter.java
+++ app/src/main/java/com/example/studdybuddy/CustomAdapter/RecentMessageAdapter.java
@@ -1,19 +1,25 @@
 package com.example.studdybuddy.CustomAdapter;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.studdybuddy.activities.ChatActivity;
 import com.example.studdybuddy.databinding.DisplayUserRecentConversationBinding;
 import com.example.studdybuddy.entity.Message;
+import com.example.studdybuddy.entity.User;
+import com.example.studdybuddy.utilities.Constants;
 
 import java.util.List;
 
 public class RecentMessageAdapter extends RecyclerView.Adapter<RecentMessageAdapter.ConversionViewHolder> {
 
     private final List<Message> chatMessages;
+
 
     public RecentMessageAdapter(List<Message> messages){
         chatMessages = messages;
@@ -53,6 +59,14 @@ public class RecentMessageAdapter extends RecyclerView.Adapter<RecentMessageAdap
         void setData(Message chatMessage) {
             binding.recentMessage.setText(chatMessage.message);
             binding.userNameRecent.setText(chatMessage.conversionName);
+            binding.getRoot().setOnClickListener(view -> {
+                User user = new User();
+                user.userName = chatMessage.conversionName;
+                user.id = chatMessage.conversionId;
+                Intent intent = new Intent(binding.getRoot().getContext(), ChatActivity.class);
+                intent.putExtra(Constants.KEY_USER, user);
+                binding.getRoot().getContext().startActivity(intent);
+            });
         }
     }
 }
